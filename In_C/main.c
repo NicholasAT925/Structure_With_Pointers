5d541339fd37024913a8fe8a82f0a439b80948ad
Zuriel <2001555@sit.singaporetech.edu.sg>
2022-05-19 20:59:44 +0800

    Update main.c

--- In_C/main.c
+++ In_C/main.c
@@ -17,18 +17,17 @@ struct intPtr{
     int *p2;
 };
 int main(){
-    /* Initializing Global Variables */
-
     /* to use pointer to dereference a struct */
     struct date today, secondDay, *ptr_today, *datePtr;
-
     ptr_today = &today;
     ptr_today->day = 5;
     ptr_today->month = 6;
     ptr_today->year = 22;
+    printf("\nTodays date is %d/%d/%.d\n\n", ptr_today->month, ptr_today->day, ptr_today->year);
+    printf("\nTodays date is %d/%d/%.d\n\n", ptr_today->month, ptr_today->day, ptr_today->year);
 
-    printf("\nTodays date is %d/%d/%.d\n\n", ptr_today->month, ptr_today->day, ptr_today->year);
-    printf("\nTodays date is %d/%d/%.d\n\n", ptr_today->month, ptr_today->day, ptr_today->year);
+
+
     /* indirect access member of structure */
     datePtr = &secondDay;
     (*datePtr).day = 15;
@@ -39,9 +38,12 @@ int main(){
     printf("printing using the '->' operator\n");
     printf("2nd day date is access through indirection and is = %d/%d/%d\n\n", datePtr->day, datePtr->month, datePtr->year);
 
+
+
+
+
     /* pointers in the struct */
     int a = 100, b;
-
     /* create struct that has pointers as members */
     struct intPtr structPointer;
     structPointer.p1 = &a;
